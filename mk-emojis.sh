ab3adc5601a652be0eae75539e18dcc9d075c9d9
walker.zheng <walker.zheng@inforun.net>
2017-07-20 15:30:27 +0800

    add github emoji png
    Signed-off-by: walker.zheng <walker.zheng@inforun.net>

--- mk-emojis.sh
+++ mk-emojis.sh
@@ -11,9 +11,9 @@ curl https://gitmoji.carloscuesta.me/index.html|grep 'class="emoji-info"'|sed 's
 # 对比 gogs & github
 comm <(cd gogs;ls|sed 's,\..*,,'|sort) <(awk '{print $1}' github_api.txt|sort)|sed 's,\t,=,g' > comm.tmp
 echo "### gogs emoji list" >> README.md
-LINE="";COUNT=0;awk -F= '{print $1}' comm.tmp|grep -v '^$'|sed 's,\(.*\),![:\1:](gogs/\1.png) `:\1:` ,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
+LINE="";COUNT=0;awk -F= '{print $1}' comm.tmp|grep -v '^$'|sed 's,\(.*\),<img src="#gogs/\1.png" width="24" height="24" alt=":\1:" /> `:\1:` ,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
 echo "### github emoji list" >> README.md
-LINE="";COUNT=0;awk -F= '{print $2}' comm.tmp|grep -v '^$'|sed 's,\(.*\),<img src="github/unicode/\1.png?v7" width="24" height="24" alt=":\1:" /> `:\1:`,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
+LINE="";COUNT=0;awk -F= '{print $2}' comm.tmp|grep -v '^$'|sed 's,\(.*\),<img src="#github/unicode/\1.png?v7" width="24" height="24" alt=":\1:" /> `:\1:`,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
 echo "### github & gogs emoji list" >> README.md
-LINE="";COUNT=0;awk -F= '{print $3}' comm.tmp|grep -v '^$'|sed 's,\(.*\),![:\1:](gogs/\1.png) `:\1:` ,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
+LINE="";COUNT=0;awk -F= '{print $3}' comm.tmp|grep -v '^$'|sed 's,\(.*\),<img src="#gogs/\1.png" width="24" height="24" alt=":\1:" /> `:\1:` ,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
 rm comm.tmp
