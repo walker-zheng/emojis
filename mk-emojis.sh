9e9472a777bc1d03f98b8953d808d6df3e6cecfd
walker.zheng <walker.zheng@inforun.net>
2017-07-20 18:15:21 +0800

    使用 github 图标(gogs的包括在内)
    Signed-off-by: walker.zheng <walker.zheng@inforun.net>

--- mk-emojis.sh
+++ mk-emojis.sh
@@ -10,10 +10,10 @@ curl https://gitmoji.carloscuesta.me/index.html|grep 'class="emoji-info"'|sed 's
 #   mkdir -p github/unicode;(awk '{print $2}' github_api.txt|sed 's,.*icons/emoji/,,;s,?.*,,'|sort)|xargs -i -P 0 wget -c https://assets-cdn.github.com/images/icons/emoji/{}?v7 -O {}
 # 对比 gogs & github
 comm <(cd gogs;ls|sed 's,\..*,,'|sort) <(awk '{print $1}' github_api.txt|sort)|sed 's,\t,=,g' > comm.tmp
-echo "### gogs emoji list" >> README.md
-LINE="";COUNT=0;awk -F= '{print $1}' comm.tmp|grep -v '^$'|sed 's,\(.*\),![:\1:](gogs/\1.png) `:\1:` ,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
+#   echo "### gogs emoji list" >> README.md
+#   LINE="";COUNT=0;awk -F= '{print $1}' comm.tmp|grep -v '^$'|sed 's,\(.*\),![:\1:](gogs/\1.png) `:\1:` ,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
 echo "### github emoji list" >> README.md
-LINE="";COUNT=0;awk -F= '{print $2}' comm.tmp|grep -v '^$'|sed 's,\(.*\),<img src="https://assets-cdn.github.com/images/icons/emoji/unicode/\1.png?v7" width="24" height="24" alt=":\1:" /> `:\1:`,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
-echo "### github & gogs emoji list" >> README.md
-LINE="";COUNT=0;awk -F= '{print $3}' comm.tmp|grep -v '^$'|sed 's,\(.*\),![:\1:](gogs/\1.png) `:\1:` ,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
+LINE="";COUNT=0;cat github_api.txt|awk '{print "<img src=\""$2"\" width=\"24\" height=\"24\" alt=\":"$1":\" /> `:"$1":` "}' |while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
+#   echo "### github & gogs emoji list" >> README.md
+#   LINE="";COUNT=0;awk -F= '{print $3}' comm.tmp|grep -v '^$'|sed 's,\(.*\),![:\1:](gogs/\1.png) `:\1:` ,'|while read line;do (( COUNT+=1 )); (( ! (COUNT % ROWS) )) && { echo " - $LINE" ; LINE=""; } || { LINE="$LINE $line"; };done >> README.md
 rm comm.tmp
